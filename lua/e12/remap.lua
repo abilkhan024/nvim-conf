a6c9f208b1adcbcda577074abb78e2c8ae1c4cad
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-09-15 15:57:03 +0600

    feat(WordWrap): toggle word wrap

--- lua/e12/remap.lua
+++ lua/e12/remap.lua
@@ -4,5 +4,7 @@ vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("n", "<leader>so", vim.cmd.so)
 vim.keymap.set("n", "<leader>c", vim.cmd.bd)
+vim.keymap.set("n", "<C-w>", "<cmd>set wrap!<CR>")
+
 
 
