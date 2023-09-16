5aad698b473d4de2a190e6939ff73cff57f903b8
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-09-16 20:02:07 +0600

    feat: add cycle back between buffers

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -2,5 +2,6 @@ require("bufferline").setup{}
 
 vim.keymap.set('n', '<leader>bb', '<CMD>BufferLinePick<CR>')
 vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
 vim.keymap.set('n', 'gs', '<CMD>BufferLineSortByDirectory<CR>')
 
