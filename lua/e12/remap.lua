3aafd02d99f50433e3a81675c84dce18acc26c73
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-09-29 22:25:43 +0600

    feat: small remaps + add comments to remaps

--- lua/e12/remap.lua
+++ lua/e12/remap.lua
@@ -1,34 +1,61 @@
-
+-- Toggle fileexplorer
 vim.keymap.set("n", "<leader>e",  "<cmd>Neotree toggle reveal<CR>")
+-- Focus fileexplorer
 vim.keymap.set("n", "<leader>o",  "<cmd>Neotree reveal<CR>")
+-- Write
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
+-- Quit vim
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
+-- Quit vim (force)
+vim.keymap.set("n", "<leader>Q", '<cmd>q!<CR>')
+-- Update file contents to recent (When git modifies file)
 vim.keymap.set("n", "<leader>r", vim.cmd.e)
-vim.keymap.set("n", "<leader>so", vim.cmd.so)
-vim.keymap.set("n", "<leader>c", vim.cmd.bd)
+-- Close current buffer
+vim.keymap.set("n", "q", vim.cmd.bd)
+-- Turnoff search highlight
 vim.keymap.set("n", "<leader>ho", vim.cmd.noh)
 
+-- When changin conf
+-- Source file
+vim.keymap.set("n", "<leader><leader>s", vim.cmd.so)
+-- Packer sync
+vim.keymap.set("n", "<leader><leader>p", '<cmd>PackerSync<CR>')
+
 vim.keymap.set("n", "<leader>pa", "<cmd>lua vim.diagnostic.open_float()<CR>")
+-- Go to prev problem
 vim.api.nvim_set_keymap('n', '<leader>pp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
+-- Go to next problem
 vim.api.nvim_set_keymap('n', '<leader>pn', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
+-- Close all buffers except current
 vim.api.nvim_set_keymap('n', '<leader>C', [[:%bd|e#|bd#<CR>'"]], { noremap = true, silent = true })
+-- Set tabsize 2
 vim.api.nvim_set_keymap('n', '<leader>sss', [[:set tabstop=2<CR>:set shiftwidth=2<CR>]], { noremap = true, silent = true })
+-- Set tabsize 4
 vim.api.nvim_set_keymap('n', '<leader>ssb', [[:set tabstop=4<CR>:set shiftwidth=4<CR>]], { noremap = true, silent = true })
+-- Toggle line wrap
 vim.keymap.set("n", "<C-w>", "<cmd>set wrap!<CR>")
 
+-- Restart langauge server
 vim.keymap.set("n", "<leader>lr", "<cmd>LspRestart<CR>")
+-- Show git status
 vim.keymap.set("n", "<leader>gt", "<cmd>Neotree float git_status<CR>")
+-- Prettier buffer
 vim.keymap.set("n", "<leader>pf", "<cmd>Neoformat<CR>")
 
+-- Move selection down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- Move selection up
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- Navigate between splits
 vim.keymap.set("n", "<c-l>", "<cmd>wincmd l<CR>")
 vim.keymap.set("n", "<c-h>", "<cmd>wincmd h<CR>")
 vim.keymap.set("n", "<c-j>", "<cmd>wincmd j<CR>")
 vim.keymap.set("n", "<c-k>", "<cmd>wincmd k<CR>")
 
+-- Buffer force delete file
 vim.api.nvim_set_keymap('n', '<leader>bfd', "<cmd>call delete(expand('%')) | bdelete!<CR>", { noremap = true, silent = true })
+-- Buffer copy relative path
 vim.keymap.set("n", "<leader>brp", function()
 	vim.api.nvim_call_function("setreg", {"+", vim.fn.fnamemodify(vim.fn.expand("%"), ":.")})
 	vim.cmd("echomsg 'Copied realtive path in clipboard'")
@@ -36,6 +63,7 @@ end, {})
 
 -- Depends on git config
 --[[
+-- git config --global --edit
 [alias]
     pb = !git push origin $(git symbolic-ref --short HEAD)
 
@@ -55,13 +83,17 @@ end, {})
     }; f"
     c = checkout
 --]]
+-- Git push branch
 vim.keymap.set("n", "<leader>gpb", "<cmd>!git pb<CR>")
+-- Git pull branch
 vim.keymap.set("n", "<leader>glb", [[:lua GitCustom('lb')<CR>]])
+-- Git yank current branch name
 vim.keymap.set("n", "<leader>gyy", function ()
 	local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
 	vim.api.nvim_call_function("setreg", {"+", branch })
 	vim.cmd("echomsg 'Copied current branch:' " .. vim.fn.string(branch))
 end)
+-- Git pull branch --no-ff (force)
 vim.keymap.set("n", "<leader>glf", [[:lua GitCustom('lbf')<CR>]])
 vim.keymap.set("n", "<leader>gc", [[:lua GitCustom('c')<CR>]])
 -- Depends on git config
