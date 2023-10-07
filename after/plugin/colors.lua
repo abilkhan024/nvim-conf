e8ada3a5169715820c03ff1f0ae0acff4f5c7ba5
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-10-07 13:37:52 +0600

    feat: add incremental selection in treesitter + some colors

---
+++ after/plugin/colors.lua
@@ -0,0 +1,8 @@
+require("catppuccin").setup({
+	styles = {
+		comments = { "italic" },
+		conditionals = { "italic" },
+		functions = { "bold", "italic" },
+    properties = { "bold", "italic" },
+	},
+})
--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -22,6 +22,15 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-	context_commentstring = { enable = true },
+  context_commentstring = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<TAB>',
+      scope_incremental = false,
+      node_incremental = '<TAB>',
+      node_decremental = '<S-TAB>',
+    },
+  },
 }
 
