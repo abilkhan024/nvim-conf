7facced96cecbbf47f45cfbde3f54fd1479d2534
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-10-08 10:31:55 +0600

    chore: remove styles for properties

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,7 +2,6 @@ require("catppuccin").setup({
 	styles = {
 		comments = { "italic" },
 		conditionals = { "italic" },
-		functions = { "bold", "italic" },
-    properties = { "bold", "italic" },
+		functions = { "bold", "italic" }
 	},
 })
