2ec2cd1a2d88e2e9863001578538161648e76399
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-10-21 11:19:25 +0600

    feat: buffer line reamps

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,7 +1,9 @@
 require("bufferline").setup{}
 
-vim.keymap.set('n', '<leader>bb', '<CMD>BufferLinePick<CR>')
+vim.keymap.set('n', '<leader>bp', '<CMD>BufferLinePick<CR>')
 vim.keymap.set('n', '<S-l>', '<CMD>BufferLineCycleNext<CR>')
 vim.keymap.set('n', '<S-h>', '<CMD>BufferLineCyclePrev<CR>')
-vim.keymap.set('n', 'gs', '<CMD>BufferLineSortByDirectory<CR>')
+vim.keymap.set('n', '<C-S-l>', '<CMD>BufferLineMoveNext<CR>')
+vim.keymap.set('n', '<C-S-h>', '<CMD>BufferLineMovePrev<CR>')
+vim.keymap.set('n', '<leader>bs', '<CMD>BufferLineSortByRelativeDirectory<CR>')
 
