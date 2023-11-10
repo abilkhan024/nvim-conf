acc1abe8299df075d366bd20f1ea44f896f660b6
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-11-10 21:50:54 +0600

    fix: volar did't attach for non standart vue repoitories

---
+++ after/plugin/lspconfig.lua
@@ -0,0 +1,30 @@
+local lspconfig = require('lspconfig')
+
+lspconfig.volar.setup({
+  filetypes = { 'typescript', 'javascript', 'vue', 'json' },
+  root_dir = function ()
+    return vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1])
+  end,
+})
+
+lspconfig.emmet_language_server.setup({
+  filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "php" },
+  init_options = {
+    --- @type string[]
+    excludeLanguages = {},
+    --- @type string[]
+    extensionsPath = {},
+    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/preferences/)
+    preferences = {},
+    --- @type boolean Defaults to `true`
+    showAbbreviationSuggestions = true,
+    --- @type "always" | "never" Defaults to `"always"`
+    showExpandedAbbreviation = "always",
+    --- @type boolean Defaults to `false`
+    showSuggestionsAsSnippets = false,
+    --- @type table<string, any> [Emmet Docs](https://docs.emmet.io/customization/syntax-profiles/)
+    syntaxProfiles = {},
+    --- @type table<string, string> [Emmet Docs](https://docs.emmet.io/customization/snippets/#variables)
+    variables = {},
+  }
+})
