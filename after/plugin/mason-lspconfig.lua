7120b3d62e3ac93842f1e77b1837606d872c9171
Amanali Abilkhan <a.abilkhan024@gmail.com>
2023-12-16 10:51:35 +0600

    chore: update list of default mason lsp's

--- after/plugin/mason-lspconfig.lua
+++ after/plugin/mason-lspconfig.lua
@@ -1,13 +1,15 @@
 require("mason-lspconfig").setup({
 	ensure_installed = {
-	 	'volar',
-		'tsserver',
-		'lua_ls',
-		'intelephense',
-		'cssls',
-		'emmet_language_server',
-		'tailwindcss',
-    'marksman',
-	}
+		"volar",
+		"tsserver",
+		"lua_ls",
+		"intelephense",
+		"cssls",
+		"emmet_language_server",
+		"tailwindcss",
+		"marksman",
+		"clangd",
+		"pint",
+		"stylua",
+	},
 })
-
